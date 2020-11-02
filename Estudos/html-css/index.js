6e41078d400488bc2beec8f633b779f4876febd9
Bragatte <marcelobragatte@gmail.com>
2020-11-02 14:07:18 -0300

    React eg

---
+++ Estudos/html-css/index.js
@@ -0,0 +1,28 @@
+<CapsLock texto="Me deixe em Caps Lock por favor"/>
+
+function CapsLock(props) {
+    const textoInserido = props.texto;
+    const textoEmCpasLock = textoInserido.toUpperCase();
+return <div>{textoEmCpasLock}</div>
+}
+
+function Pagina() {
+    return <div>Contador Massinha</div>
+}
+
+import {useState} from 'react';
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+    
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
